7a69c698eb498f565b34cbfeae583deab60b3f9c
Chloe Lam <21168@mychs.ca>
2020-12-29 19:52:33 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chloe-web/precache-manifest.d5b5e60d1f81dc2b2c84e85bcd21164b.js"
+  "/chloe-web/precache-manifest.f0ec0896a1a387293498fc960b1014a7.js"
 );
 
 self.addEventListener('message', (event) => {
