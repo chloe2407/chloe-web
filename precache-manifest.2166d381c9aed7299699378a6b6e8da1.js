09719eb7e24b5f0ccba29fd3a059b95519819295
Chloe Lam <21168@mychs.ca>
2021-04-10 18:51:49 -0700

    Updates

---
+++ precache-manifest.2166d381c9aed7299699378a6b6e8da1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c32084c4e025e1655de4bf86f2b1e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3267f43d0b1ce692b05a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "862414f1be0adadc93c6",
+    "url": "/static/js/2.9b5d97ae.chunk.js"
+  },
+  {
+    "revision": "74a29ea35ba8e5c14153137d2ba2fa70",
+    "url": "/static/js/2.9b5d97ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3267f43d0b1ce692b05a",
+    "url": "/static/js/main.1fb14f6e.chunk.js"
+  },
+  {
+    "revision": "dafa5e3e8d0c2b37ce89",
+    "url": "/static/js/runtime-main.0d4575ab.js"
+  },
+  {
+    "revision": "004a8b1f872958e5bf63ec54cf2acdbf",
+    "url": "/static/media/4dprint.004a8b1f.png"
+  },
+  {
+    "revision": "0f892553b6a56f09aa88766cf1f4febc",
+    "url": "/static/media/aboutme.0f892553.jpg"
+  },
+  {
+    "revision": "68bb73199fdd9b3a412b359e573c4ec1",
+    "url": "/static/media/ai_article.68bb7319.jpeg"
+  },
+  {
+    "revision": "45dbc8e4d9dc4763805036629fb82d28",
+    "url": "/static/media/brainer.45dbc8e4.png"
+  },
+  {
+    "revision": "7b4e156c10a91cad902ef2813eb3851d",
+    "url": "/static/media/citychallenge.7b4e156c.png"
+  },
+  {
+    "revision": "e545791e0a1f28a1d9fd80826588fdea",
+    "url": "/static/media/craftingsjoy.e545791e.png"
+  },
+  {
+    "revision": "e63431426e8d672d5ebb429e2b8a1024",
+    "url": "/static/media/growth_article.e6343142.jpeg"
+  },
+  {
+    "revision": "00bdaadfd10d5aab270ae971c97924bb",
+    "url": "/static/media/hapticai_op.00bdaadf.png"
+  },
+  {
+    "revision": "6144eaa4e1be7be4cd6d1c0226ac0625",
+    "url": "/static/media/hapticai_vid.6144eaa4.png"
+  },
+  {
+    "revision": "d508d5001c83fbfa30b446099d016a34",
+    "url": "/static/media/longevitypitch.d508d500.png"
+  },
+  {
+    "revision": "1250fb3aeb5ca9399164efd1c6adb9c1",
+    "url": "/static/media/myicon.1250fb3a.png"
+  },
+  {
+    "revision": "d4b6f90a5e52ac2bbf15fff706e0bc05",
+    "url": "/static/media/name.d4b6f90a.png"
+  },
+  {
+    "revision": "36ed64a5ce6206981326281b6e9edfc8",
+    "url": "/static/media/nanotech.36ed64a5.png"
+  },
+  {
+    "revision": "e78ab32e29c937b278a39b5a5a354d03",
+    "url": "/static/media/newsletter.e78ab32e.png"
+  },
+  {
+    "revision": "d972746d9864d3a0ace3bd01bf81989b",
+    "url": "/static/media/penguin.d972746d.png"
+  },
+  {
+    "revision": "57b60c1eaa6c1944b1ded5e983d955f0",
+    "url": "/static/media/pic.57b60c1e.jpg"
+  },
+  {
+    "revision": "7866d31eb9e5025c11fa4c0b4b7909af",
+    "url": "/static/media/planit.7866d31e.png"
+  },
+  {
+    "revision": "a0e087dac0e9492e007eda3b80a70a3a",
+    "url": "/static/media/psiquantum.a0e087da.png"
+  },
+  {
+    "revision": "a5fa64d3f5b3591ccdf5ef0f558dd6d0",
+    "url": "/static/media/resume.a5fa64d3.pdf"
+  },
+  {
+    "revision": "2459df152b545a7b84d371eeda284cce",
+    "url": "/static/media/stemcells.2459df15.png"
+  },
+  {
+    "revision": "ffe51acf93b947788cbbfaa28909c5f5",
+    "url": "/static/media/unchallenge.ffe51acf.png"
+  },
+  {
+    "revision": "9d36a6e1ad4be4ce6537b7497c69f2e7",
+    "url": "/static/media/upcyc.9d36a6e1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67238e35284e454feeea6f13d299ae73.js"
+  "/precache-manifest.2166d381c9aed7299699378a6b6e8da1.js"
 );
 
 self.addEventListener('message', (event) => {
